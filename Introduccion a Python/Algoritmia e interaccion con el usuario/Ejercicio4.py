edee85c9d8cbe96b47925a25d47ebc98b6cc6b0e
JBruno <juanbruno.fernandez.pessoa@iessanmamede.com>
2026-09-21 10:38:12 +0200

    MODIFICACION: Realizado el ejercicio 4

--- Introduccion a Python/Algoritmia e interaccion con el usuario/Ejercicio4.py
+++ Introduccion a Python/Algoritmia e interaccion con el usuario/Ejercicio4.py
@@ -3,4 +3,17 @@
 print("Introduce una operacion básica (+ - / *): ", end="")
 operacion= input()
 operacion = operacion.strip().replace(" ","")
-operator_position= operacion.find("+" or "-" or "/" or "*")
+
+numbers= operacion.split("+" or "-" or "/" or "*")
+first_number= int(numbers[0])
+second_number= int(numbers[1])
+
+match operacion[operacion.find("+" or "-" or "/" or "*")]:
+    case "+":
+        print(f"Su suma es: {first_number+second_number} ")
+    case "-":
+        print(f"Su resta es: {first_number-second_number} ")
+    case "*":
+        print(f"Su multiplicación es: {first_number*second_number} ")
+    case "/":
+        print(f"Su división es: {first_number/second_number} ")
