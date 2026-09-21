80b74e2f208cefb338279a3beaa28683dde92994
JBruno <juanbruno.fernandez.pessoa@iessanmamede.com>
2026-09-21 09:49:47 +0200

    Ejercicio4 modificado

--- Introduccion a Python/Algoritmia e interaccion con el usuario/Ejercicio4.py
+++ Introduccion a Python/Algoritmia e interaccion con el usuario/Ejercicio4.py
@@ -1,18 +1,6 @@
-""""Ordenacion de palabras"""
-print("Ingresa ua frase: ", end="")
-phrase= input()
-sorted_phrase_abc= sorted(phrase.split(), key=str.casefold)
-sorted_phrase_123= sorted(phrase.split(), key=str.__len__)
-ordered_phrase_abc=""
-ordered_phrase_123=""
+""" Calculadora básica """
 
-for word in sorted_phrase_abc:
-    ordered_phrase_abc += word + " "
-ordered_phrase_abc = ordered_phrase_abc.strip()
-
-for word in sorted_phrase_123:
-    ordered_phrase_123 += word + " "
-ordered_phrase_123 = ordered_phrase_123.strip()
-
-print(f"Su frase ordenada alfabeticamente es: {ordered_phrase_abc}")
-print(f"Su frase ordenada alfabeticamente es: {ordered_phrase_123}")
+print("Introduce una operacion básica (+ - / *): ", end="")
+operacion= input()
+operacion = operacion.strip().replace(" ","")
+operator_position= operacion.find("+" or "-" or "/" or "*")
