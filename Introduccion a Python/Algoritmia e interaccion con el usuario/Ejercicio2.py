10c40fc164f118c9881522d6b49337fcf7ce1cc6
JBruno <juanbruno.fernandez.pessoa@iessanmamede.com>
2026-09-21 09:58:07 +0200

    CORRECION: Ejercicio 2

--- Introduccion a Python/Algoritmia e interaccion con el usuario/Ejercicio2.py
+++ Introduccion a Python/Algoritmia e interaccion con el usuario/Ejercicio2.py
@@ -8,3 +8,12 @@ for number in numbers:
     list.append(int(number))
 for number in list:
     print(f"{number} es par" if number % 2 == 0 else f"{number} es impar")
+
+
+""" Correcion """
+
+numeros = list(map(int, input("Introduce una lista de números separados por espacios: ").split()))
+pares=[n for n in numeros if n%2==0]
+impares=[n for n in numeros if n%2!=0]
+
+print(f"Pares: {pares}\nImpares: {impares}")
