bfc2b8cccb6177953001fc3292946842704cda44
JBruno <juanbruno.fernandez.pessoa@iessanmamede.com>
2026-09-22 09:41:35 +0200

    MODIFICACION: Realizacion del ejercicio 8

--- Introduccion a Python/Algoritmia e interaccion con el usuario/Ejercicio8.py
+++ Introduccion a Python/Algoritmia e interaccion con el usuario/Ejercicio8.py
@@ -1,4 +1,16 @@
 """ JUEGO DE ADIVINANZA   """
 import random
 
-random()
+secret_number= random.randint(0, 100)
+answer = -1
+print(secret_number)
+
+while answer != secret_number:
+    print("Introduce un número entre 0 y 100: ", end= "")
+    answer= int(input())
+    if answer < secret_number:
+        print(f"Es mayor.")
+    elif answer > secret_number:
+        print(f"Es menor.")
+
+print("¡¡¡ Enhorabuena, GANASTE !!!")
